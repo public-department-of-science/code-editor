1d43acd392ffccd3abb808542b99922a4b5331e6
bohdan skrypka <skripkabodya@gmail.com>
2022-07-03 16:33:21 +0300

    - add code-highlight styles for my custom grammar-syntax highlighting

--- Code.Editor/Code.Editor/CodeEditorMainForm/FunctionalMenuStrip.cs
+++ Code.Editor/Code.Editor/CodeEditorMainForm/FunctionalMenuStrip.cs
@@ -3,6 +3,7 @@ using FarsiLibrary.Win;
 using FastColoredTextBoxNS;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Code.Editor
@@ -165,7 +166,6 @@ namespace Code.Editor
                 documentMap.Target = newTextBox;
                 newTextBox.WordWrap = true;
                 newTextBox.ShowCaretWhenInactive = true;
-                newTextBox.CustomAction += closeOpenTabByHotKey_CustomAction;
                 newTextBox.Font = new Font("Consolas", 9.75f);
                 newTextBox.ContextMenuStrip = codeAreaContextMenu;
                 newTextBox.Dock = DockStyle.Fill;
@@ -193,12 +193,14 @@ namespace Code.Editor
                 newTextBox.Focus();
                 newTextBox.DelayedTextChangedInterval = 1000;
                 newTextBox.DelayedEventsInterval = 500;
+                newTextBox.CustomAction += closeOpenTabByHotKey_CustomAction;
                 newTextBox.TextChangedDelayed += new EventHandler<TextChangedEventArgs>(tb_TextChangedDelayed);
                 newTextBox.SelectionChangedDelayed += new EventHandler(tb_SelectionChangedDelayed);
                 newTextBox.KeyDown += new KeyEventHandler(tb_KeyDown);
                 newTextBox.MouseMove += new MouseEventHandler(tb_MouseMove);
                 newTextBox.MouseDown += new MouseEventHandler(tb_MouseDown);
-                newTextBox.ToolTipNeeded += new EventHandler<ToolTipNeededEventArgs>(NewTextBox_ToolTipNeeded);
+                newTextBox.TextChanged += new EventHandler<TextChangedEventArgs>(tb_TextChanged);
+                newTextBox.ToolTipNeeded += new EventHandler<ToolTipNeededEventArgs>(tb_ToolTipNeeded);
                 newTextBox.ChangedLineColor = changedLineColor;
                 if (buttonHighlightCurrentLine.Checked)
                 {
@@ -243,7 +245,64 @@ namespace Code.Editor
             }
         }
 
-        private void NewTextBox_ToolTipNeeded(object sender, ToolTipNeededEventArgs e)
+        #region Code highlighting
+
+        TextStyle BlueStyle = new TextStyle(Brushes.Blue, null, FontStyle.Regular);
+        TextStyle BoldStyle = new TextStyle(null, null, FontStyle.Bold);// | FontStyle.Underline);
+        TextStyle GrayStyle = new TextStyle(Brushes.Gray, null, FontStyle.Regular);
+        TextStyle MagentaStyle = new TextStyle(Brushes.Magenta, null, FontStyle.Regular);
+        TextStyle GreenStyle = new TextStyle(Brushes.Green, null, FontStyle.Italic);
+        TextStyle BrownStyle = new TextStyle(Brushes.Brown, null, FontStyle.Italic);
+        TextStyle MaroonStyle = new TextStyle(Brushes.Maroon, null, FontStyle.Regular);
+        MarkerStyle SameWordsStyle = new MarkerStyle(new SolidBrush(Color.FromArgb(40, Color.Gray)));
+
+        private void tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CurrentTextBox.LeftBracket = '(';
+            CurrentTextBox.RightBracket = ')';
+            CurrentTextBox.LeftBracket2 = '{';
+            CurrentTextBox.RightBracket2 = '}';
+            //clear style of changed range
+            e.ChangedRange.ClearStyle(BlueStyle, BoldStyle, GrayStyle, MagentaStyle, GreenStyle, BrownStyle);
+
+            //string highlighting
+            e.ChangedRange.SetStyle(BrownStyle, @"""""|@""""|''|@"".*?""|(?<!@)(?<range>"".*?[^\\]"")|'.*?[^\\]'");
+            //comment highlighting
+            e.ChangedRange.SetStyle(GreenStyle, @"//.*$", RegexOptions.Multiline);
+            e.ChangedRange.SetStyle(GreenStyle, @"(/\*.*?\*/)|(/\*.*)", RegexOptions.Singleline);
+            e.ChangedRange.SetStyle(GreenStyle, @"(/\*.*?\*/)|(.*\*/)", RegexOptions.Singleline | RegexOptions.RightToLeft);
+            //number highlighting
+            e.ChangedRange.SetStyle(MagentaStyle, @"\b\d+[\.]?\d*([eE]\-?\d+)?[lLdDfF]?\b|\b0x[a-fA-F\d]+\b");
+            //attribute highlighting
+            e.ChangedRange.SetStyle(GrayStyle, @"^\s*(?<range>\[.+?\])\s*$", RegexOptions.Multiline);
+            //class name highlighting
+            e.ChangedRange.SetStyle(BoldStyle, @"\b(class|struct|enum|interface)\s+(?<range>\w+?)\b");
+
+            //keyword highlighting
+            var reservedKeywords = new StringBuilder();
+            reservedKeywords.Append(@"\b(");
+            foreach (string keyword in keywords)
+            {
+                reservedKeywords.Append(keyword);
+                reservedKeywords.Append("|");
+            }
+            reservedKeywords.Append(@")\b");
+
+            e.ChangedRange.SetStyle(BlueStyle, reservedKeywords.ToString());
+            // @"\b()\b|#region\b|#endregion\b");
+
+            //clear folding markers
+            e.ChangedRange.ClearFoldingMarkers();
+
+            //set folding markers
+            e.ChangedRange.SetFoldingMarkers("{", "}");//allow to collapse brackets block
+            e.ChangedRange.SetFoldingMarkers(@"#region\b", @"#endregion\b");//allow to collapse #region blocks
+            e.ChangedRange.SetFoldingMarkers(@"/\*", @"\*/");//allow to collapse comment block
+        }
+
+        #endregion
+
+        private void tb_ToolTipNeeded(object sender, ToolTipNeededEventArgs e)
         {
             if (!string.IsNullOrEmpty(e.HoveredWord))
             {
